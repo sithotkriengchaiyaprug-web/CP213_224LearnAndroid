a5af539f8b281dc6e2b45bfd7e51c1ac4ebc6832
sithotkriengchaiyaprug-web <sithot.kriengchaiyaprug@g.swu.ac.th>
2026-01-27 15:06:46 +0700

    update activity2

--- Lab/app/build.gradle.kts
+++ Lab/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.a224_lablearnandroid"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.a224_lablearnandroid"
         minSdk = 30
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
