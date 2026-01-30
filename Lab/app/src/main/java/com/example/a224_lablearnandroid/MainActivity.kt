42917add350fca9be1a618bb42f9601cd7031e86
sithotkriengchaiyaprug-web <sithot.kriengchaiyaprug@g.swu.ac.th>
2026-01-30 11:14:25 +0700

    .

--- Lab/app/src/main/java/com/example/a224_lablearnandroid/MainActivity.kt
+++ Lab/app/src/main/java/com/example/a224_lablearnandroid/MainActivity.kt
@@ -3,45 +3,54 @@ package com.example.a224_lablearnandroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.a224_lablearnandroid.ui.theme._224_LabLearnAndroidTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            _224_LabLearnAndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = Color.Gray)
+                    .padding(32.dp)
+            ) {
+                // ส่วนของ HP Bar (พื้นหลังสีขาว)
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(32.dp)
+                        .background(color = Color.White)
+                ) {
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth(0.24f)
+                            .fillMaxSize()
+                            .background(color = Color.Red)
+                    )
+
+
+                    Text(
+                        text = "HP: 24/100",
+                        modifier = Modifier
+                            .align(alignment = Alignment.Center)
+                            .padding(horizontal = 8.dp),
+                        color = Color.Black
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    _224_LabLearnAndroidTheme {
-        Greeting("Android")
     }
 }
--- Lab/app/src/main/java/com/example/a224_lablearnandroid/MainActivity2.kt
+++ Lab/app/src/main/java/com/example/a224_lablearnandroid/MainActivity2.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.a224_lablearnandroid.ui.theme._224_LabLearnAndroidTheme
@@ -18,13 +24,13 @@ class MainActivity2 : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-//            _224_LabLearnAndroidTheme {
+            _224_LabLearnAndroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting2(
                         name = "Android2",
                         modifier = Modifier.padding(innerPadding)
                     )
-//                }
+                }
             }
         }
     }
@@ -32,10 +38,20 @@ class MainActivity2 : ComponentActivity() {
 
 @Composable
 fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    var inputText by remember { mutableStateOf("") }
+
+    Column(modifier = modifier) {
+        Text(
+            text = "Hello $name! say = $inputText"
+        )
+        TextField(
+            value = inputText, // ต้องเป็นตัว v พิมพ์เล็ก (ไม่ใช่ Value)
+            onValueChange = { newValue ->
+                inputText = newValue // อัปเดตค่าเมื่อพิมพ์
+            },
+            label = { Text("กรอกข้อความที่นี่") }
+        )
+    }
 }
 
 @Preview(showBackground = true)
